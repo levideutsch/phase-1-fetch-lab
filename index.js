cc1e152062bcf28b00c06a1de7a4e36f591284e5
levideutsch <levideutsch@live.com>
2022-10-06 22:57:08 -0400

    commit tree f2b3ad57822076a57f6dc4004c81a1c8baf48d57

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+ return  fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -11,7 +12,9 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+fetchBooks()
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks()
+// });
